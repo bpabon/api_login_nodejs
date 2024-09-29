8bad547dd472569427e893513bcc136b90ac9452
unknown <bpabon1@udi.edu.co>
2024-09-29 16:51:51 -0500

    Inicio documentación con swagger

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,23 @@
 const express = require('express');
 const authRouter = require('./auth/auth.routes');
 const publicRouter = require('./public/changePassword.routes');
-
+/**
+* @swagger
+* /user:
+*   get:
+*     tags:
+*       - user
+*     summary: Lista los usuarios aldair
+*     responses:
+*       200:
+*         description: Lista de usuarios
+*         content:
+*           application/json:
+*             schema:
+*               type: array
+*               items:
+*                 $ref: '#/definitions/User'
+*/
 function routerApi(app) {
     const router = express.Router();
     app.use('/api/v1', router);
